8877cc5ce8dcf1250c361cc551d417903b353e1b
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-11 14:02:25 -0300

    Criação de controller

--- src/controllers/ToolsController.js
+++ src/controllers/ToolsController.js
@@ -1,19 +1,36 @@
+const Tool = require('../database/model/Tool');
+
+const ArrayToString = require('../utils/ArrayToString');
+
 module.exports = {
   async index(req, res) {
-    const tools = await connection('tools').select('*');
+    const tools = await Tool.find();
     return res.json(tools);
   },
+
   async create(req, res) {
-    const { title, link, description } = req.body;
-
-    const id = generateUniqueId();
-
-    await connection('tools').insert({
-      id,
+    const { title, link, description, tags } = req.body;
+    const TagsArray = ArrayToString(tags);
+    
+    const ToolResponse = await Tool.create({
       title,
       link,
-      description
+      description,
+      tags: TagsArray
     })
-    return res.json({ id })
+    return res.status(201).json(ToolResponse);
+  },
+
+  async show(req, res) {
+    const { tag } = req.query;
+    const ToolTag = await Tool.find({
+      tags: tag});
+      return res.json(ToolTag);
+  },
+
+  async destroy(req, res) {
+    const { id } = req.params;
+    await Tool.findOneAndDelete({_id: id})
+    res.send();
   }
 };
