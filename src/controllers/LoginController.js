23ed300f942b74ad3b9cab13187b123b22d96356
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-11 16:47:38 -0300

    Criação de controller de login

---
+++ src/controllers/LoginController.js
@@ -0,0 +1,26 @@
+const User = require('../database/model/User');
+
+const bcrypt = require('bcryptjs');
+
+const generateToken = require('../utils/generateToken');
+
+module.exports = {
+  async index(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user) {
+      return res.status(400).json({error: 'User not exits'});
+    }
+    if(!await bcrypt.compare(password, user.password)) {
+      return res.status(400).json({error: 'Password Invalid'});
+    }
+    user.password = undefined;
+
+    return res.status(200).json({
+      user, 
+      token: generateToken({id: user.id})
+    });
+  }
+};
