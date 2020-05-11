9cecf1dcc0a99e2cbd05f436862920c0753b5460
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-11 14:02:08 -0300

    Criação de modelo

---
+++ src/database/model/Tool.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const ToolSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String],
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Tool', ToolSchema);
