95a193b37adfe70c7da334f77850c88a4574d3cd
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-12 11:21:33 -0300

    Mudança no arquivo config

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.json');
+const authConfig = require('../config/auth');
 
 module.exports = function generateToken(params = {}){
     return jwt.sign(params, authConfig.secret, {
