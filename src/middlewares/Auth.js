1847ecec2bd46b4c68c7e13a2d4215da3cf550c0
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-12 10:33:52 -0300

    Pequena mudanças

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -1,30 +1,31 @@
- const jwt = require('jsonwebtoken');
-  const authConfig = require('../config/auth.json');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json');
 
-  module.exports = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+module.exports = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-      return res.status(401).send({ error: 'No token provided' });
+  if (!authHeader) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  const parts = authHeader.split(' ');
+
+  if (!parts.length === 2) {
+    return res.status(401).json({ error: 'Token error' });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).json({ error: 'Token malformated' });
+  }
+
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Token invalid' });
     }
+    req.userId = decoded.id;
 
-    const parts = authHeader.split(' ');
-
-    if (!parts.length === 2) {
-      return res.status(401).send({ error: 'Token error' });
-    }
-
-    const [scheme, token] = parts;
-
-    if (!/^Bearer$/i.test(scheme)) {
-      return res.status(401).send({ error: 'Token malformated' });
-    }
-
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
-      if (err) res.status(401).send({ error: 'Token invalid' });
-
-      req.userId = decoded.id;
-
-      return next();
-    });
-  };
+    return next();
+  });
+};
