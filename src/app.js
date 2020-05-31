defb514de2f90db790534f080c5825ec13722388
Matheus Santos Moreira <matheussm301@gmail.com>
2020-05-31 11:03:14 -0300

    Eslint

--- src/app.js
+++ src/app.js
@@ -2,21 +2,21 @@ require('dotenv').config()
 require('./database/connection')
 
 const express = require('express')
-const logger = require('morgan')
 const cors = require('cors')
+const logger = require('morgan')
 
 const Routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
+
 app.set('port', port)
+app.use(Routes)
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-app.use(cors())
-app.use(Routes)
 
 app.listen(port, () => console.log(`> [Server] start in Port ${port}`))
